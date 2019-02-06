03335151dcb76c61be4246cc41fd0d674bc6251a
d4n0n_myself <danon.sibaev@gmail.com>
2019-02-07 00:14:51 +0300

    ThreadImpl - failed
    => 2 times slower than 1-thread impl

--- ExcelTestFramework/Program.cs
+++ ExcelTestFramework/Program.cs
@@ -1,98 +1,146 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelTestFramework
 {
     class Program
     {
+        private static Excel._Worksheet Worksheet = null;
+        private static object locker = new object();
+        private static StreamWriter stream = null;
+        #region Example
+        static int x = 0;
+        static void Main1(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+
+            Console.ReadLine();
+        }
+        public static void Count()
+        {
+            lock (locker)
+            {
+                x = 1;
+                for (int i = 1; i < 9; i++)
+                {
+                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                    x++;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            //Example();
-            //MyImplementation();
-            WriteFileFunction();
-        }
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-
-        private static void WriteFileFunction()
-        {
             var excelApp = new Excel.Application();
             excelApp.Visible = true;
             excelApp.Workbooks.Open(@"C:\Users\danon\Desktop\qwerty\example");
-            Excel._Worksheet workSheet = excelApp.ActiveSheet;
-
-            using (var stream = File.AppendText(@"C:\Users\danon\Desktop\qwerty\output.txt"))
+            //Excel._Worksheet workSheet = excelApp.ActiveSheet; for sync way
+            Worksheet = excelApp.ActiveSheet;
+            using (var writer = File.AppendText(@"C:\Users\danon\Desktop\qwerty\output.txt"))
             {
-                for (var i = 2; i < 957384; i++)
-                {
-                    string value = Convert.ToString(workSheet.get_Range($"B{i}", Type.Missing).Value2);
-                    int index;
-                    if (value.IndexOf("д.") != -1 && !Char.IsNumber(value[value.IndexOf("д.") + 2]))
-                        index = value.IndexOf("д.");
-                    else if (value.IndexOf("пгт.") != -1)
-                        index = value.IndexOf("пгт.") + 2;
-                    else
-                    {
-                        index = value.IndexOf("г.") == -1 ?
-                               value.IndexOf("с.") == -1 ?
-                                   value.IndexOf("п.") : value.IndexOf("с.")
-                           : value.IndexOf("г.");
-                    }
-
-                    if (index <= 0)
-                        continue;
-                    index = index + 2;
-
-                    stream.Write(workSheet.get_Range($"A{i}", Type.Missing).Value2 + ";" + 
-                        value.Substring(index).Replace("д.", "").Replace("ул.", "").Replace("кв.", "") + "\r\n");
-                }
+                stream = writer;
+                // synchronous way
+                ProcessAndWriteCells(new Range(2, 10000));
             }
+            // asynchronous way
+            //WriteFileFunctionWithThreads(10000);
+
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed (in ms) : " + stopwatch.ElapsedMilliseconds);
             Console.WriteLine("Ready to go");
             Console.ReadLine();
         }
 
-        private static void MyImplementation()
+        /*private static void WriteFileFunctionWithThreads(int cellsCount)
         {
-            var excelApp = new Excel.Application();
-            excelApp.Visible = true;
-
-            excelApp.Workbooks.Open(@"C:\Users\danon\Desktop\qwerty\1234");
-            Excel._Worksheet workSheet = excelApp.ActiveSheet;
-            List<dynamic> completeValues = new List<dynamic>();
-            string a;
-            for (var i = 1; i < 16; i++)
+            var countStep = cellsCount / 4;
+            Thread[] additionalThreads = {
+                new Thread(new ParameterizedThreadStart(Process)),
+                new Thread(new ParameterizedThreadStart(Process)),
+                new Thread(new ParameterizedThreadStart(Process))
+            };
+            var currentStep = countStep;
+            for (var i = 0; i < additionalThreads.Length; i++)
             {
-                string value = Convert.ToString(workSheet.get_Range($"A{i}", Type.Missing).Value2);
-                File.AppendAllText(@"C:\Users\danon\Desktop\qwerty\output.txt", workSheet.get_Range($"A{i}", Type.Missing).Value2 + "\r\n");
+                additionalThreads[i].Start(new Range(currentStep, currentStep + countStep, i+2));
+                currentStep += countStep;
             }
-
-            Console.WriteLine("Ready to go");
-            Console.ReadLine();
+            
+            ProcessAndWriteCells(new Range(2, countStep));
         }
 
-        static void Example()
+        private static void Process(object obj)
         {
-            var excelApp = new Excel.Application();
+            Range range = null;
+            //try
+            //{
+                range = (Range)obj;
 
-            excelApp.Visible = true;
+                var check = range.StartingCellIndex;
+            //}
+            //catch (Exception e)
+            //{
+            //Console.WriteLine("Unexpected range\r\n");
+            //Console.WriteLine(e.Message);
+            // return;
+            //}
+            ProcessAndWriteCells(range);
+        }*/
 
-            //excelApp.Workbooks.Add();
+        private static void ProcessAndWriteCells(Range range)
+        { 
+            for (var i = 2; i < 10001; i++)
+            {
+                string value = Convert.ToString(Worksheet.get_Range($"B{i}", Type.Missing).Value2);
+                var address = ProcessStringAddress(value);
 
+                if (address == string.Empty)
+                    continue;
 
+                //lock (locker)
+                //{
+                    
+                        stream.WriteLine(Worksheet.get_Range($"A{i}", Type.Missing).Value2 + ";" + address);
+                    
+               // }
 
-            excelApp.Workbooks.Open(@"C:\Users\danon\Desktop\qwerty\1234");
-            Excel._Worksheet workSheet = excelApp.ActiveSheet;
-            workSheet.Cells[1, "A"] = "ID Number";
+                //Console.WriteLine($"Value processed by thread : {range.HandlingThreadNumber}");
+            }
+        }
 
-            string a;
-            var excelcells = workSheet.get_Range("A1", Type.Missing);
-            a = Convert.ToString(excelcells.Value2);
+        private static string ProcessStringAddress(string value)
+        {
+            int index;
+            if (value.IndexOf("д.") != -1 && !Char.IsNumber(value[value.IndexOf("д.") + 2]))
+                index = value.IndexOf("д.");
+            else if (value.IndexOf("пгт.") != -1)
+                index = value.IndexOf("пгт.") + 2;
+            else
+            {
+                index = value.IndexOf("г.") == -1 ?
+                       value.IndexOf("с.") == -1 ?
+                           value.IndexOf("п.") : value.IndexOf("с.")
+                   : value.IndexOf("г.");
+            }
 
-            Console.WriteLine("a1=" + a);
+            if (index <= 0)
+                return "";
+            index = index + 2;
 
-
-            Console.ReadLine();
+            return value.Substring(index).Replace("д.", "").Replace("ул.", "").Replace("кв.", "");
         }
     }
 }
---
+++ ExcelTestFramework/Range.cs
@@ -0,0 +1,18 @@
+﻿namespace ExcelTestFramework
+{
+    class Range
+    {
+        public Range(int start, int end, int threadNumber = 1)
+        {
+            StartingCellIndex = start;
+            EndingCellIndex = end;
+            HandlingThreadNumber = threadNumber;
+        }
+
+        public int StartingCellIndex { get; }
+        public int EndingCellIndex { get; }
+
+        // unnecessary property, purpose: more information in console
+        public int HandlingThreadNumber { get; } 
+    }
+}
