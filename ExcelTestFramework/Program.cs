0b5b6088b7e63baee046fc142393d084e7e397d4
d4n0n_myself <danon.sibaev@gmail.com>
2019-02-07 00:26:39 +0300

    refactoring after ThreadImpl
    => stored in branch, not faster than usual

--- ExcelTestFramework/Program.cs
+++ ExcelTestFramework/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using Excel = Microsoft.Office.Interop.Excel;
 
@@ -9,11 +9,16 @@ namespace ExcelTestFramework
     {
         static void Main(string[] args)
         {
-            //Example();
-            //MyImplementation();
-            WriteFileFunction();
-        }
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
+            WriteFileFunction();
+
+            stopwatch.Stop();
+            Console.WriteLine("Time elapsed (in ms) : " + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Ready to go");
+            Console.ReadLine();
+        }
 
         private static void WriteFileFunction()
         {
@@ -24,75 +29,39 @@ namespace ExcelTestFramework
 
             using (var stream = File.AppendText(@"C:\Users\danon\Desktop\qwerty\output.txt"))
             {
-                for (var i = 2; i < 957384; i++)
+                for (var i = 2; i < 10000; i++)
                 {
                     string value = Convert.ToString(workSheet.get_Range($"B{i}", Type.Missing).Value2);
-                    int index;
-                    if (value.IndexOf("д.") != -1 && !Char.IsNumber(value[value.IndexOf("д.") + 2]))
-                        index = value.IndexOf("д.");
-                    else if (value.IndexOf("пгт.") != -1)
-                        index = value.IndexOf("пгт.") + 2;
-                    else
-                    {
-                        index = value.IndexOf("г.") == -1 ?
-                               value.IndexOf("с.") == -1 ?
-                                   value.IndexOf("п.") : value.IndexOf("с.")
-                           : value.IndexOf("г.");
-                    }
+                    var address = ProcessStringAddress(value);
 
-                    if (index <= 0)
+                    if (address == string.Empty)
                         continue;
-                    index = index + 2;
 
-                    stream.Write(workSheet.get_Range($"A{i}", Type.Missing).Value2 + ";" + 
-                        value.Substring(index).Replace("д.", "").Replace("ул.", "").Replace("кв.", "") + "\r\n");
+                    stream.Write(workSheet.get_Range($"A{i}", Type.Missing).Value2 + ";" + address + "\r\n");
                 }
             }
-            Console.WriteLine("Ready to go");
-            Console.ReadLine();
         }
 
-        private static void MyImplementation()
+        private static string ProcessStringAddress(string value)
         {
-            var excelApp = new Excel.Application();
-            excelApp.Visible = true;
-
-            excelApp.Workbooks.Open(@"C:\Users\danon\Desktop\qwerty\1234");
-            Excel._Worksheet workSheet = excelApp.ActiveSheet;
-            List<dynamic> completeValues = new List<dynamic>();
-            string a;
-            for (var i = 1; i < 16; i++)
+            int index;
+            if (value.IndexOf("д.") != -1 && !Char.IsNumber(value[value.IndexOf("д.") + 2]))
+                index = value.IndexOf("д.");
+            else if (value.IndexOf("пгт.") != -1)
+                index = value.IndexOf("пгт.") + 2;
+            else
             {
-                string value = Convert.ToString(workSheet.get_Range($"A{i}", Type.Missing).Value2);
-                File.AppendAllText(@"C:\Users\danon\Desktop\qwerty\output.txt", workSheet.get_Range($"A{i}", Type.Missing).Value2 + "\r\n");
+                index = value.IndexOf("г.") == -1 ?
+                       value.IndexOf("с.") == -1 ?
+                           value.IndexOf("п.") : value.IndexOf("с.")
+                   : value.IndexOf("г.");
             }
 
-            Console.WriteLine("Ready to go");
-            Console.ReadLine();
-        }
+            if (index <= 0)
+                return "";
+            index = index + 2;
 
-        static void Example()
-        {
-            var excelApp = new Excel.Application();
-
-            excelApp.Visible = true;
-
-            //excelApp.Workbooks.Add();
-
-
-
-            excelApp.Workbooks.Open(@"C:\Users\danon\Desktop\qwerty\1234");
-            Excel._Worksheet workSheet = excelApp.ActiveSheet;
-            workSheet.Cells[1, "A"] = "ID Number";
-
-            string a;
-            var excelcells = workSheet.get_Range("A1", Type.Missing);
-            a = Convert.ToString(excelcells.Value2);
-
-            Console.WriteLine("a1=" + a);
-
-
-            Console.ReadLine();
+            return value.Substring(index).Replace("д.", "").Replace("ул.", "").Replace("кв.", "");
         }
     }
 }
